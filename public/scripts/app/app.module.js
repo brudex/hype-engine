53a99717e1d6802e47785b697f565080441c9142
AkotechIntelligence <brudexgh@gmail.com>
2026-02-24 21:36:28 +0000

    hide angular tags while page is loading

--- public/scripts/app/app.module.js
+++ public/scripts/app/app.module.js
@@ -1,4 +1,11 @@
-﻿(function() {
+(function() {
     angular
-        .module('app', []);
+        .module('app', [])
+        .run(['$timeout', function($timeout) {
+            // Remove dashboard loading state after Angular has bootstrapped and compiled.
+            // This hides the progress bar and reveals the app, avoiding flash of uncompiled {{ }} templates.
+            $timeout(function() {
+                document.body.classList.remove('dashboard-loading');
+            }, 0);
+        }]);
 })();
